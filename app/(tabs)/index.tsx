9e18aca3ea6a04d498f837beb105b4211961d8ca
David Wilding <wilderv3@gmail.com>
2023-12-18 18:26:19 -0800

    add Paper

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,14 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { Button } from 'react-native-paper';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
